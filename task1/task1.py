25e461cd94cf3d7e326879ffeb73ce3b56a96b86
vaka <ivanka6342@gmail.com>
2019-04-14 09:50:03 +0300

    wrote a bash script for searching and executing C&Py-code

---
+++ task1/task1.py
@@ -0,0 +1,7 @@
+#!/home/vaka/.pyenv/shims/python3
+
+from sys import argv
+
+scriptName, length = argv
+
+print ("%s arg: %s\n" % (scriptName, length))
--- task1/task1.sh
+++ task1/task1.sh
@@ -3,9 +3,9 @@
 cSource=$1
 pyScript=$2
 
-#find ~/
+scriptsDir=$(find ~/ -name "$cSource" -execdir [ -e "$pyScript" ] \; -printf %h\\n)
 
-gcc $cSource -o taskC
+gcc ${scriptsDir}/${cSource} -o taskC
 chmod u+x $pyScript
 
 while [ -n "${3}" ]
@@ -13,8 +13,8 @@ do
     echo -e "\nC-code execution result(arg = $3):"
     ./taskC ${3}
     
-    echo -e "\nPy-code execution result(arg = $3):""
-    python3 ./$pyScript ${3}
+    echo -e "\nPy-code execution result(arg = $3):"
+    python3 ${scriptsDir}/${pyScript} ${3}
 
     shift
 done
